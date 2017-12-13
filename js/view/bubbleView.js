ce6a597789b9ef3252ccbc8dffb8b907303e9f11
Maria Westling <kemwestling@gmail.com>
2017-12-13 19:07:51 +0100

    no message

--- js/view/bubbleView.js
+++ js/view/bubbleView.js
@@ -22,9 +22,10 @@ var BubbleView = function(container, model) {
 
   this.update = function() {
     newSpeed = model.convertHeartRate();
-    // console.log(document.getElementById("bubbleVideo").playbackRate += newSpeed);
+    // console.log(document.getElementById("bubbleVideo").playbackRate);
     if (0.7 <= (document.getElementById("bubbleVideo").playbackRate + newSpeed) <= 2.3){
       document.getElementById("bubbleVideo").playbackRate += newSpeed;
+      console.log(document.getElementById("bubbleVideo").playbackRate);
 
     }
   }
