27d32c011e25a74c951be2ec13e257dfdf52b424
Maria Westling <kemwestling@gmail.com>
2017-12-14 12:20:01 +0100

    no message

--- js/view/smokeView.js
+++ js/view/smokeView.js
@@ -11,7 +11,7 @@ var SmokeView = function(container, model) {
 
   this.update = function() {
     this.newSpeed = model.convertHeartRate();
-    console.log("tree new speed");
+    console.log("smoke new speed", this.newSpeed);
     this.currentSpeed = document.getElementById("smokeVideo").playbackRate;
     // console.log(document.getElementById("bubbleVideo").playbackRate);
     if (0.7 <= (this.currentSpeed + this.newSpeed) <= 2.3){
